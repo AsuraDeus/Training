ffa9ddeb3d9d7b0159dda949e63bd9a673aaf8e6
Stanislav <asuradeus@gmail.com>
2017-08-04 22:05:36 +0000

    Home folder

---
+++ Home/first.php
@@ -0,0 +1,56 @@
+========== 03.08 ==========
+<br />
+<?php
+echo "Hi, World";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+ <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque, ut perferendis possimus quam provident maxime deserunt accusamus quidem odit. Aliquid modi reprehenderit architecto molestias commodi. Ea quos praesentium molestiae expedita.</div>  
+</body>
+</html>
+<?php
+// phpinfo();
+echo "Wait wut<br>";
+echo "<strong>Wait wut</strong><br /><br />";
+echo '<strong>Wait wut</strong><br /><br />';
+?>
+
+
+<?php
+echo '<input type="text" name="name"><br /><br />'; //Одинарные кавычку ускоряют работу скрипта
+echo "<input type=\"text\" name=\"name\"><br /><br />";
+?>
+
+====================
+<br />
+<?php
+$text = 'hello world';
+?>
+
+<input type="text" value="<?php echo $text;?>">
+<br />
+
+====================
+<br />
+<?php
+//ini_set('error_reporting','E_ALL'); //Настройка ини файла через код
+echo $var = 'Alex';
+$var2 = 'Billy';
+echo "<br />";
+echo "$var2";
+?>
+<br />
+====================
+
+<br />
+<?php
+
+?>
+<br />
